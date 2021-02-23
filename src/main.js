506f0f46c1edd09f9b1d1b628a85f2e49c2577f5
DESKTOP-KDCM8IF\HOTELGA <bomnarain@hotelga.co.kr>
2021-02-23 18:53:45 +0900

    전체

---
+++ src/main.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import VeeValidate from 'vee-validate';
+import { Validator } from 'vee-validate';
+import KoreanValidate from 'vee-validate/dist/locale/ko'
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+  faHome,
+  faUser,
+  faUserPlus,
+  faSignInAlt,
+  faSignOutAlt
+} from '@fortawesome/free-solid-svg-icons';
+
+
+library.add(faHome, faUser, faUserPlus, faSignInAlt, faSignOutAlt);
+
+Vue.config.productionTip = false;
+
+// 한글 세팅
+Validator.localize('ko', KoreanValidate)
+Vue.use(VeeValidate, { locale: KoreanValidate });
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/register',
+    component: Register
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    // lazy-loaded
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    // lazy-loaded
+    component: () => import('../views/BoardAdmin.vue')
+  },
+  {
+    path: '/mod',
+    name: 'moderator',
+    // lazy-loaded
+    component: () => import('../views/BoardModerator.vue')
+  },
+  {
+    path: '/user',
+    name: 'user',
+    // lazy-loaded
+    component: () => import('../views/BoardUser.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
